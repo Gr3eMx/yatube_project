b6e4e61cb0835b9d58586156fcfa014ea6f15869
Максим Ильченко <maximhp79@mail.ru>
2022-04-18 11:10:07 +0300

    Добавлены urls и view-функции

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,10 +4,9 @@ from . import views
 urlpatterns = [
     # Главная страница
     path('', views.index),
-    # Список мороженого
+
     path('group/', views.group_posts),
-    # Подробная информация о мороженом. Ждем пременную типа int,
-    # и будем использовать ее под именем pk
+
     path(
         'group/<slug:pk>/',
         views.post_detail
