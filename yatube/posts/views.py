90a00e346edb8f6949ce5e9b1f69779bdd328eb4
Максим Ильченко <maximhp79@mail.ru>
2022-04-19 10:11:53 +0300

    Разбил html страничку на блоки.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,16 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
-
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request):
-    return HttpResponse('Список постов')
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 
 
