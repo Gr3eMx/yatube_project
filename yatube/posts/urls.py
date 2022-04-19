2ae29e91295a8d4f768abde64ce820cf1aa2d70a
Максим Ильченко <maximhp79@mail.ru>
2022-04-19 18:43:21 +0300

    Добавил иконки и стили.

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name = 'index'),
 
-    path('group/', views.group_posts),
+    path('group/', views.group_posts, name = 'group_posts'),
 
     path(
         'group/<slug:pk>/',
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,12 +5,20 @@ from django.shortcuts import render
 
 # Главная страница
 def index(request):
+    text = 'Это главная страница проекта Yatube'
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 def group_posts(request):
+    text = 'Здесь будет информация о группах проекта Yatube'
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 
